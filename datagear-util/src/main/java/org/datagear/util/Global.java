e850ff87f828e12cc4edd52e060f48ae425ee89b
龚 崇金 <gdzsgcj@126.com>
2020-12-17 17:01:53 +0800

    修改了相关产品名称和网站链接等

--- datagear-util/src/main/java/org/datagear/util/Global.java
+++ datagear-util/src/main/java/org/datagear/util/Global.java
@@ -11,6 +11,7 @@ package org.datagear.util;
  * 全局常量。
  * 
  * @author datagear@163.com
+ * @author gdgzg modify 2020-12-17
  *
  */
 public final class Global
@@ -24,11 +25,12 @@ public final class Global
 	public static final String VERSION = "2.0.0";
 
 	/** 中文产品名称 */
-	public static final String PRODUCT_NAME_ZH = "数据齿轮";
-
+//	public static final String PRODUCT_NAME_ZH = "数据齿轮";
+	public static final String PRODUCT_NAME_ZH = "南方所数据魔方";
 	/** 英文产品名称 */
-	public static final String PRODUCT_NAME_EN = "DataGear";
-
+//	public static final String PRODUCT_NAME_EN = "DataGear";
+	public static final String PRODUCT_NAME_EN = "NfsDataCube";
 	/** 官网 */
-	public static final String WEB_SITE = "http://www.datagear.tech";
+//	public static final String WEB_SITE = "http://www.datagear.tech";
+	public static final String WEB_SITE = "http://www.smei.net.cn";
 }
